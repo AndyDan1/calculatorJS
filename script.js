c843c683dc94af80a51961192c5535768c66f322
Andy <itflenovo@gmail.com>
2022-08-11 20:52:47 +0300

    working project calculator

---
+++ script.js
@@ -0,0 +1,87 @@
+let a = ''//first number
+let b = ''//second number
+let sign = ''// operation sign
+let finish = false
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+const action = ['-', '+', '*', '/', '+/-']
+const out = document.querySelector('.calc-screen p')
+
+//! clear parameters
+const clearAll = () => {
+   a = ''
+   b = ''
+   sign = ''
+   finish = false
+   out.textContent = 0
+}
+document.querySelector('.ac').onclick = clearAll
+
+document.querySelector('.buttons').onclick = (event) => {
+   // нажата не кнопка
+   if (!event.target.classList.contains('btn')) return
+   // нажата кнопка clearAll
+   if (event.target.classList.contains('ac')) return
+
+   out.textContent = ''
+   // получаю нажатую кнопку
+   const key = event.target.textContent
+   // если нажата цифровая кнопка 0-9 .
+   if (digit.includes(key)) {
+      if (b === '' && sign === '') {
+         a += key
+         out.textContent = a
+      } else if (a !== '' && b !== '' && finish) {
+         b = key
+         finish = false
+         out.textContent = b
+      } else {
+         b += key
+         out.textContent = b
+      }
+      return
+   }
+
+   // если нажата кнопка + - / * 
+   if (action.includes(key)) {
+      sign = key
+      out.textContent = sign
+      return
+   }
+
+   //нажата =
+   if (key === '=') {
+      if (b === '') b = a
+      switch (sign) {
+         case "+":
+            a = (+a) + (+b)
+            break;
+         case "-":
+            a = (+a) - (+b)
+            break;
+         case "*":
+            a = (+a) * (+b)
+            break;
+         case "/":
+            if (b === '0') {
+               out.textContent = 'Ошибка'
+               a = ''
+               b = ''
+               sign = ''
+               return
+            }
+            a = (+a) / (+b)
+            break;
+         case "+/-":
+            if (a > 0 || a < 0) {
+               a = -a
+               out.textContent = a
+               return
+            }
+            break
+      }
+      finish = true
+      out.textContent = a
+   }
+}
+console.log(10*0.12);
